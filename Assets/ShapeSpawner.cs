46b71a527102f77986b5bd0257c4ce95a31c5665
Sakhawat Hossain <shossain@idare.io>
2022-10-15 22:22:33 +0600

    Mission Complete

---
+++ Assets/ShapeSpawner.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+public class ShapeSpawner : PoolerBase<Shape> {
+    [SerializeField] private Shape _shapePrefab;
+    [SerializeField] private int _spawnAmount = 20;
+    
+
+    private void Start() {
+        InitPool(_shapePrefab); // Initialize the pool
+
+        // var shape = Get(); // Pull from the pool
+        // Release(shape); // Release back to the pool
+        InvokeRepeating(nameof(Spawn), 0.2f, 0.2f);
+    }
+
+    private void Spawn()
+    {
+        for (int i = 0; i < _spawnAmount; i++)
+        {
+            Shape shape = Get();
+            shape.Init(Killshape);
+        }
+    }
+
+    private void Killshape(Shape shape)
+    {
+        Release(shape);
+    }
+
+    // Optionally override the setup components
+    protected override void GetSetup(Shape shape) {
+        base.GetSetup(shape);
+        shape.transform.position = Vector3.zero;
+    }
+}
